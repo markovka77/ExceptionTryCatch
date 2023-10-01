c0999e9522170bceeb1e0d2470355ea266643ecc
Mark <voronkov1mark@gmail.com>
2023-10-01 05:29:05 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         int[] arr = new int[c];
         try{
-            arr[b]=23;
-//            int[] arr2 = new int[a];
+//            arr[b]=23;
+           int[] arr2 = new int[a];
 
         }catch (IndexOutOfBoundsException | NegativeArraySizeException i){
         System.out.println("Warning");}
